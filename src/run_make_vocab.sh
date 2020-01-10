d4883e5e47f0e93b46f3e1f8cf5bb6da42020b5f
Trang Tran <trangham283@users.noreply.github.com>
2020-01-10 12:53:21 -0800

    Update run_make_vocab.sh

--- src/run_make_vocab.sh
+++ src/run_make_vocab.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-split=$1
-dtype=$2
+split=$1 # train,valid,test
+dtype=$2 # clean, disf, dtok
 DATA_DIR=/g/ssli/projects/disfluencies/ttmt001
-SENT_FILE=${DATA_DIR}/${split}_list
+SENT_FILE=${DATA_DIR}/${split}_list 
+# list of files in each split, you can get this by `ls ${split} >> sent_file` or something like that
 
 #FILES=`ls ${DATA_DIR}`
 #for F in $FILES 
